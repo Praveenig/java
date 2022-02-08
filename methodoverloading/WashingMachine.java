01bfe9f24f69574144aaee4df12e75ffd3af97b0
Praveenig <praveeng.xworkz@gmail.com>
2022-02-08 13:32:32 +0530

    method

---
+++ methodoverloading/WashingMachine.java
@@ -0,0 +1,13 @@
+class washingMachine{
+
+string name;
+int price;
+String color;
+String type;
+String dimension;
+}
+
+public  String WashingClothes(){
+System.out.println("WashingClothes for family Members");
+ }
+}
---
+++ methodoverloading/WashingMachineUtil.java
@@ -0,0 +1,23 @@
+class WashingMachineUtil{
+
+
+public static void main(String a[]){
+
+	WashingMachine washingMachine= new WashingMachine();
+	washingMachine.name="LG";
+	washingMachine.price=6000;
+	washingMachine.color="Blue";
+	washingMachine.type="Front Load";
+	washingMachine.dimension="1123*78 mm";
+	washingMachine.WashingClothes();
+	
+	System.out.println(washingMachine.name +" "+ washingMachine.price +" "+washingMachine.color +" "+);
+	
+	0WashingMachine washingMachine1= new WashingMachine();
+	washingMachine1.name="Godraj";
+	washingMachine1.price=9000;
+	washingMachine1.color="Red";
+	washingMachine1.type="Side Load";
+	washingMachine1.dimension="112*78 mm";
+	}
+}
