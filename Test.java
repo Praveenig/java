d7dcc76c39076dbba3afd6a5e7788ed4bd879d4a
Praveenig <98796803+Praveenig@users.noreply.github.com>
2022-04-08 17:12:12 +0530

    weekly friday test

---
+++ Test.java
@@ -0,0 +1,78 @@
+1. we must use static and final
+    static datatype identifier name= identifier
+
+2. there are two types are in java 
+	1 primitive datatype.
+	2 non-primitive dataype.
+
+3. if the data type of a variable is known at compile time. This means that you must specify the type of the declare the veriable.
+
+4. Naming convention for java
+	Uppercase
+	Lowercase
+
+5. specifying an initial value to assign to it.
+
+6. combining all the similar type data in one unit.
+
+7. no.
+
+8. in the heap area.
+
+9. Advantages of Array
+	> Easily accessing elements using the index. 
+	> Reduce the time comlexity.
+	> it will helpfull for metrix.
+
+10. 
+
+11. class Array{
+
+	public static void main(String a[]){
+		String elements={"one","Two","Three","Four"};
+		for(int a=0; a=elements.lenght;i++)
+		System.out.println(+elements[i]);
+	}
+} 	
+12. 
+13.
+14.
+
+15. If we don't initialize the numbers in array, it can be anything. 
+    Using this instead saves to having to loop over the array and assign every value to 0. 
+
+16. 
+17. static keyword = This is pericular member to belongs to the types of itself .
+	static veriable =static veriable is also called as class variable and class initialization is initializing staic variable
+	static method = static method is belongsto the class only,and rather than instance of class.
+	
+18. yes.
+ 
+19. The JVM can ivoking it without instastiating the class.
+
+20. This is set of instruction that run only one when class is loaded in into memory.
+
+21. No.
+
+22. No.
+
+23. Calling one Constructor to another Constructor.
+
+24. This is special method,is used to initialize objects.
+	there are two type 
+	1. default Constructor.
+	2. Parameterized Constructor.
+	
+25. 
+
+26. yes.
+
+27. Using extend Keyword.
+
+28. class is blueprint which we use objects ,
+	instance is asingle or unique of the class.
+
+29. six way.
+  
+30. instantiateing a class.
+	
